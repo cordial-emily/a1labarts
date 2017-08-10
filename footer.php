754ed7a0935fff4d4315ee4708a9b846c2e28b3a
Emily Schleiner <cordial.emily@gmail.com>
2017-08-09 17:13:39 -0700

    padding above giving assistant badge

--- footer.php
+++ footer.php
@@ -53,7 +53,7 @@
 				<span class="site-title"><a class="one" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>  <a class="two" href="https://www.facebook.com/A1LabArts/"> Like Us on <span class="dashicons dashicons-facebook"></span> </a> <a class="two" href="http://cordial-emily.com/">Design</a> <a class="three" href="mailto:contact@a1labarts.com?Subject=Hello%20A1labarts">Contact Us</a></span>
 				<a href="http://www.grassroots.org" data-mce-href="http://www.grassroots.org"><img src="http://www.a1labarts.com/wp-content/uploads/2016/06/Grassroots.org-logo.2.png" class="grassroots" alt="grassroots.org logo" data-mce-src="http://www.grassroots.org/newwp/wp-content/uploads/2015/09/logo-square.png" /></a>
 				<a style="text-align:center;" href="http://tnartscommission.org/"><img src="http://www.a1labarts.com/wp-content/uploads/2016/05/tac.1.png" alt="Tennessee Arts Commission logo" /></a>
-				<a style="text-align:center;" href="https://givingassistant.org/np#a1labarts"><img src="http://www.a1labarts.com/wp-content/uploads/2017/08/giving_assistant_givva_4.png" alt="Giving Assistant" /></a>
+				<a style="text-align:center; padding-top:40px;" href="https://givingassistant.org/np#a1labarts"><img src="http://www.a1labarts.com/wp-content/uploads/2017/08/giving_assistant_givva_4.png" alt="Giving Assistant" /></a>
 			</div><!-- .site-info -->
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
