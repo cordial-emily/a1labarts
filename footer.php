1a8735f634e21f5464aa3eb705caa995ec84e7ff
Emily Schleiner <cordial.emily@gmail.com>
2017-08-10 11:47:19 -0700

    rework footer with giving assist badge

--- footer.php
+++ footer.php
@@ -17,29 +17,39 @@
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
 			<?php if ( has_nav_menu( 'primary' ) ) : ?>
-				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
+				<nav class="main-navigation site-info" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
 					<?php
 						wp_nav_menu( array(
 							'theme_location' => 'primary',
-							'menu_class'     => 'primary-menu',
+							'menu_class'     => 'primary-menu site-title',
 						 ) );
 					?>
 				</nav><!-- .main-navigation -->
 			<?php endif; ?>
 
 			<?php if ( has_nav_menu( 'social' ) ) : ?>
-				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
+				<nav class="social-navigation site-info" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
 					<?php
 						wp_nav_menu( array(
 							'theme_location' => 'social',
 							'menu_class'     => 'social-links-menu',
 							'depth'          => 1,
-							'link_before'    => '<span class="screen-reader-text">',
+							'link_before'    => '<span class="screen-reader-text site-title">',
 							'link_after'     => '</span>',
 						) );
 					?>
 				</nav><!-- .social-navigation -->
 			<?php endif; ?>
+			<nav class="main-navigation site-info">
+				<div class="menu-top-menu-container site-title">
+					<ul class="primary-menu">
+						<li class="menu-item menu-item-type-post_type menu-item-object-page"><a class="one" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+						<li><a href="https://www.facebook.com/A1LabArts/"> Like Us on <span class="dashicons dashicons-facebook"></span> </a></li>
+						<li><a href="https://paralleldesign.net/">Design</a></li>
+						<li><a href="mailto:contact@a1labarts.com?Subject=Hello%20A1labarts">Contact Us</a></li>
+					</ul>
+				</div>
+			</nav>
 
 			<div class="site-info">
 				<?php
@@ -50,11 +60,27 @@
 					 */
 					do_action( 'twentysixteen_credits' );
 				?>
-				<span class="site-title"><a class="one" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>  <a class="two" href="https://www.facebook.com/A1LabArts/"> Like Us on <span class="dashicons dashicons-facebook"></span> </a> <a class="two" href="http://cordial-emily.com/">Design</a> <a class="three" href="mailto:contact@a1labarts.com?Subject=Hello%20A1labarts">Contact Us</a></span>
-				<a href="http://www.grassroots.org" data-mce-href="http://www.grassroots.org"><img src="http://www.a1labarts.com/wp-content/uploads/2016/06/Grassroots.org-logo.2.png" class="grassroots" alt="grassroots.org logo" data-mce-src="http://www.grassroots.org/newwp/wp-content/uploads/2015/09/logo-square.png" /></a>
-				<a style="text-align:center;" href="http://tnartscommission.org/"><img src="http://www.a1labarts.com/wp-content/uploads/2016/05/tac.1.png" alt="Tennessee Arts Commission logo" /></a>
-				<a style="text-align:center; padding-top:40px;" href="https://givingassistant.org/np#a1labarts"><img src="http://www.a1labarts.com/wp-content/uploads/2017/08/giving_assistant_givva_4.png" alt="Giving Assistant" /></a>
+				<!--<span class="site-title">
+					<a class="one" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					<a class="two" href="https://www.facebook.com/A1LabArts/"> Like Us on <span class="dashicons dashicons-facebook"></span> </a>
+					<a class="two" href="http://cordial-emily.com/">Design</a>
+					<a class="three" href="mailto:contact@a1labarts.com?Subject=Hello%20A1labarts">Contact Us</a>
+			  </span>-->
+				<a class="givingassist" href="https://givingassistant.org/np#a1labarts"><img src="http://www.a1labarts.com/wp-content/uploads/2017/08/giving_assistant_givva_4.png" alt="Giving Assistant" /></a>
+				<!--<div class="" style="width:100%;">
+					<a style="display: block;margin: 1.75em auto;width: 50%;float: right;" href="http://www.grassroots.org" data-mce-href="http://www.grassroots.org"><img src="http://www.a1labarts.com/wp-content/uploads/2016/06/Grassroots.org-logo.2.png" class="grassroots" alt="grassroots.org logo" data-mce-src="http://www.grassroots.org/newwp/wp-content/uploads/2015/09/logo-square.png" /></a>
+					<a style="display: block;margin: 1.75em auto;width: 50%;float: left;" href="http://tnartscommission.org/"><img src="http://www.a1labarts.com/wp-content/uploads/2016/05/tac.1.png" alt="Tennessee Arts Commission logo" /></a>
+				</div> -->
 			</div><!-- .site-info -->
+			<nav class="main-navigation site-info">
+				<div class="menu-top-menu-container site-title">
+					<ul class="primary-menu">
+						<li><a href="http://www.grassroots.org" data-mce-href="http://www.grassroots.org"><img src="http://www.a1labarts.com/wp-content/uploads/2016/06/Grassroots.org-logo.2.png" class="grassroots" alt="grassroots.org logo" data-mce-src="http://www.grassroots.org/newwp/wp-content/uploads/2015/09/logo-square.png" /></a></li>
+						<li><a href="http://tnartscommission.org/"><img src="http://www.a1labarts.com/wp-content/uploads/2016/05/tac.1.png" alt="Tennessee Arts Commission logo" /></a></li>
+					</ul>
+				</div>
+			</nav>
+			<h6 class="footer-copyr">&copy; 1995 – <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></h6>
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
 </div><!-- .site -->
